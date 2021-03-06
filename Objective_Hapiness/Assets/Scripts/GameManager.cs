e2e6dbbe7584182c5cecb579f177d5fdd6adff5c
Oscar Metten <oscar.metten@gmail.com>
2021-03-06 12:39:20 +0100

    GameManager et début héritage résident

---
+++ Objective_Hapiness/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+
+    // The instance allows all variables declared in the game manager
+    // to exist only once in the scene and remain unique.
+    // The tilemap is that of the black tiles which are the displacement tiles and
+    // the dictionnary contains a position vector associated with a node having the same position vector.
+    public static GameManager Instance;
+    public Tilemap tilemap;
+    public Dictionary<Vector3Int, Node> dictio = new Dictionary<Vector3Int, Node>();
+
+    //World settings
+    private int timeworld;
+    public bool day;
+
+    #endregion
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Objective_Hapiness/Assets/Scripts/Lumberjack.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class Lumberjack : H_Resident
+{
+    private Vector3Int forest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
