a09c98ed7eb7e9fd458b97bea148f52d9e337c72
Romain.C <romain130197@gmail.com>
2021-03-07 13:37:49 +0100

    Camera fonctionnel

--- Objective_Hapiness/Assets/Scripts/CameraController.cs
+++ Objective_Hapiness/Assets/Scripts/CameraController.cs
@@ -3,47 +3,57 @@
 public class CameraController : MonoBehaviour
 {
     public float speed;
+    public float scrollSpeed;
     float screenBorderDeviation = 10f;
 
-    bool move = true;
+    public Vector3 pannelLimite;
+    float fieldOfView = 141f;
 
-    public Vector2 pannelLimite;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > pannelLimite.x || transform.position.x < -pannelLimite.x)
-        {
-            move = !move;
-        }
-        if (!move)
-        {
-            return;
-        }
+        Vector3 position = transform.position;
+
 
         if (Input.mousePosition.y >= Screen.height - screenBorderDeviation)
         {
-            transform.Translate(0, 1 * speed * Time.deltaTime, 0, Space.World);
+            position.y += 1 * speed * Time.deltaTime;
+            //transform.Translate(0, 1 * speed * Time.deltaTime, 0, Space.World);
         }
 
         if (Input.mousePosition.y <= screenBorderDeviation)
         {
-            transform.Translate(0, -1 * speed * Time.deltaTime, 0, Space.World);
+            position.y -= 1 * speed * Time.deltaTime;
+            //transform.Translate(0, -1 * speed * Time.deltaTime, 0, Space.World);
         }
 
         if (Input.mousePosition.x >= Screen.width - screenBorderDeviation)
         {
-            transform.Translate(1 * speed * Time.deltaTime, 0, 0, Space.World);
+            position.x += 1 * speed * Time.deltaTime;
+            //transform.Translate(1 * speed * Time.deltaTime, 0, 0, Space.World);
         }
 
         if (Input.mousePosition.x <= screenBorderDeviation)
         {
-            transform.Translate(-1 * speed * Time.deltaTime, 0, 0, Space.World);
-        }    
+            position.x -= 1 * speed * Time.deltaTime;
+            //transform.Translate(-1 * speed * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        position.z += scroll * scrollSpeed * Time.deltaTime;
+        //Camera.main.fieldOfView += scroll * scrollSpeed * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, -pannelLimite.x, pannelLimite.x);
+        position.y = Mathf.Clamp(position.y, -pannelLimite.y, pannelLimite.y);
+        position.z = Mathf.Clamp(position.z, -pannelLimite.z, pannelLimite.z);
+        //Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 138f, 144f);
+
+        transform.position = position;
     }
 }
