df774d5b3bf2162d6f1412db755451ad22e2194f
Romain.C <romain130197@gmail.com>
2021-03-09 14:11:44 +0100

    Scrolling Orthographic

--- Objective_Hapiness/Assets/Scripts/CameraController.cs
+++ Objective_Hapiness/Assets/Scripts/CameraController.cs
@@ -7,12 +7,19 @@ public class CameraController : MonoBehaviour
     float screenBorderDeviation = 10f;
 
     public Vector3 pannelLimite;
-    float fieldOfView = 141f;
+    //float fieldOfView = 141f;
+
+    // For scrolling Orthographic
+    private Camera cam;
+    private float targetZoom;
+    public float ZoomLerpSpeed;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
     }
 
     // Update is called once per frame
@@ -20,7 +27,7 @@ public class CameraController : MonoBehaviour
     {
         Vector3 position = transform.position;
 
-
+        // Move the camera when the mouse is in a corner
         if (Input.mousePosition.y >= Screen.height - screenBorderDeviation)
         {
             position.y += 1 * speed * Time.deltaTime;
@@ -45,14 +52,25 @@ public class CameraController : MonoBehaviour
             //transform.Translate(-1 * speed * Time.deltaTime, 0, 0, Space.World);
         }
 
+        
+        // Scrolling for Camera Perspective
         float scroll = Input.GetAxis("Mouse ScrollWheel");
-        position.z += scroll * scrollSpeed * Time.deltaTime;
-        //Camera.main.fieldOfView += scroll * scrollSpeed * Time.deltaTime;
+        //position.z += scroll * scrollSpeed * Time.deltaTime;
+        // Second method
+         //Camera.main.fieldOfView += scroll * scrollSpeed * Time.deltaTime;
 
+        // Scrolling for Camera Orthographic
+        targetZoom -= scroll * scrollSpeed;
+        targetZoom = Mathf.Clamp(targetZoom, 2f, 5f); // Clamp for Camera Orthographic
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * ZoomLerpSpeed);
+
+        // Clamp between two value the position on the different axes
         position.x = Mathf.Clamp(position.x, -pannelLimite.x, pannelLimite.x);
         position.y = Mathf.Clamp(position.y, -pannelLimite.y, pannelLimite.y);
-        position.z = Mathf.Clamp(position.z, -pannelLimite.z, pannelLimite.z);
-        //Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 138f, 144f);
+
+        // Clamp for scrolling Camera Perspective
+        //position.z = Mathf.Clamp(position.z, -pannelLimite.z, pannelLimite.z);
+            //Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 138f, 144f);
 
         transform.position = position;
     }
