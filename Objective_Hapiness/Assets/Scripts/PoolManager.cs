43082613f034d7602366a743f04a2c897fea3ef3
Oscar Metten <oscar.metten@gmail.com>
2021-03-10 11:50:38 +0100

    Auto stash before merge of "main" and "origin/main"

--- Objective_Hapiness/Assets/Scripts/PoolManager.cs
+++ Objective_Hapiness/Assets/Scripts/PoolManager.cs
@@ -6,6 +6,7 @@ public class PoolManager : MonoBehaviour
 
     [SerializeField] GameObject hobo;
 
+
     private void Awake()
     { if (Instance == null)
         { Instance = this; }
@@ -15,15 +16,58 @@ public class PoolManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 50; i++)
-        {
-            Instantiate(hobo, transform);
-        }
+        SpawnResidents();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SpawnResidents()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(hobo, transform);
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            switch (i)
+            {
+                case 0 :
+                    GameObject Builder = Instantiate(hobo, transform);
+                    Builder.name = "Builder";
+                    Builder.tag = "Builder";
+                    Destroy(Builder.GetComponent<H_Resident>());
+                    Builder.AddComponent<Builder>();
+                    Builder.SetActive(true);
+                    break;
+                case 1 : 
+                    GameObject Harvester = Instantiate(hobo, transform);
+                    Harvester.name = "Harvester";
+                    Harvester.tag = "Harvester";
+                    Destroy(Harvester.GetComponent<H_Resident>());
+                    Harvester.AddComponent<Harvester>();
+                    Harvester.SetActive(true);
+                    break;
+                case 2 :
+                    GameObject Lumberjack = Instantiate(hobo, transform);
+                    Lumberjack.name = "Lumberjack";
+                    Lumberjack.tag = "Lumberjack";
+                    Destroy(Lumberjack.GetComponent<H_Resident>());
+                    Lumberjack.AddComponent<Lumberjack>();
+                    Lumberjack.SetActive(true);
+                    break;
+                case 3 :
+                    GameObject Minor = Instantiate(hobo, transform);
+                    Minor.name = "Minor";
+                    Minor.tag = "Minor";
+                    Destroy(Minor.GetComponent<H_Resident>());
+                    Minor.AddComponent<Minor>();
+                    Minor.SetActive(true);
+                    break;
+            }
+        }
     }
 }
